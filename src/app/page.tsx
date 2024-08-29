996f6275e760a6f7868bb4fa9ee00b6205982391
developerhost <kanonhara0609@gmail.com>
2024-08-29 10:45:47 +0900

    feat: エラーバウンダリーを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { api, HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
 import UserCardList from "./_components/user/UserCardList";
+import { Suspense } from "react";
 
 export default async function Home() {
   const users = await api.user.getUserList();
@@ -8,8 +10,16 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black to-blue-950 text-white">
         <div className="container mx-auto p-4">
-          <h1 className="mb-6 text-2xl font-bold">ユーザー一覧</h1>
-          <UserCardList users={users} />
+          <ErrorBoundary
+            fallback={
+              <div>エラーが発生しました。ページをリロードしてください。</div>
+            }
+          >
+            <Suspense fallback={<div>Loading...</div>}>
+              <h1 className="mb-6 text-2xl font-bold">ユーザー一覧</h1>
+              <UserCardList users={users} />
+            </Suspense>
+          </ErrorBoundary>
         </div>
       </main>
     </HydrateClient>
