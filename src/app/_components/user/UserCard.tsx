5c07981f2faaddb119fb693b1bb198ee87df3c72
developerhost <kanonhara0609@gmail.com>
2024-08-29 18:46:09 +0900

    feat: カードの見た目を修正

--- src/app/_components/user/UserCard.tsx
+++ src/app/_components/user/UserCard.tsx
@@ -9,14 +9,14 @@ type UserCardProps = {
 
 export function UserCard({ name, image }: UserCardProps) {
   return (
-    <Card className="mx-auto max-w-sm shadow-lg">
+    <Card className="mx-auto w-[200px] bg-transparent pt-2 shadow-lg">
       <CardContent className="flex items-center space-x-4">
         <Avatar>
           <AvatarImage src={image} alt="avatar" />
           <AvatarFallback>{name.charAt(0)}</AvatarFallback>
         </Avatar>
         <div>
-          <CardTitle className="text-lg font-bold">{name}</CardTitle>
+          <CardTitle className="text-lg font-bold text-white">{name}</CardTitle>
         </div>
       </CardContent>
     </Card>
