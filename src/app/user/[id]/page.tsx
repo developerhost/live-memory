5e558a00eb0ab3de1c7c235e89d62ecd60464bb0
developerhost <kanonhara0609@gmail.com>
2024-08-30 19:23:57 +0900

    feat: プロフィール画面に画像と名前を表示

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,64 +1,33 @@
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import Container from "@/app/_components/Container";
+import { api } from "@/trpc/server";
+import { notFound } from "next/navigation";
+import Image from "next/image";
 
-export default function CardWithForm() {
+interface UserPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function UserPage({ params }: UserPageProps) {
+  const user = await api.user.getUserById({ id: params.id });
+
+  if (!user) {
+    return notFound();
+  }
+
   return (
     <Container>
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>プロフィール編集</CardTitle>
-          <CardDescription>
-            オリジナルのプロフィールを作成できます
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">名前</Label>
-                <Input id="name" placeholder="ライブ太郎" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework">Framework</Label>
-                <Select>
-                  <SelectTrigger id="framework">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="next">Next.js</SelectItem>
-                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                    <SelectItem value="astro">Astro</SelectItem>
-                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
-        </CardFooter>
-      </Card>
+      <Image
+        src={user.image ?? ""}
+        alt="User Image"
+        width={200}
+        height={200}
+        className="mx-auto rounded-full"
+      />
+      <h2 className="text-center text-3xl font-bold">{user.name}</h2>
     </Container>
   );
 }
