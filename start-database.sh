e7ab60672f46ca708f5acaa3dd6c58930aed4765
developerhost <kanonhara0609@gmail.com>
2024-11-23 19:33:03 +0900

    first commit

--- start-database.sh
+++ start-database.sh
@@ -9,7 +9,7 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME="live-memory-postgres"
+DB_CONTAINER_NAME="1-16384-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -50,6 +50,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=live-memory \
+  -e POSTGRES_DB=1-16384 \
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
